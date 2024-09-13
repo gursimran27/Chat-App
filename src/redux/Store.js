650c01dfb6facb61c228d3485bb74dd6b0ad19d6
Gursimran27 <singhgursimran165@gmail.com>
2024-09-13 22:18:13 +0530

    redux devtool

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,24 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch as useAppDispatch, useSelector as useAppSelector } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import { rootPersistConfig, rootReducer } from './rootReducer';//for persistant storage of states even after reload or tabClose
-
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  useDispatch as useAppDispatch,
+  useSelector as useAppSelector,
+} from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { rootPersistConfig, rootReducer } from "./rootReducer"; //for persistant storage of states even after reload or tabClose
 
 const store = configureStore({
-    reducer: persistReducer(rootPersistConfig, rootReducer),
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: false,
-        immutableCheck: false,
-      }),
-  });
+  reducer: persistReducer(rootPersistConfig, rootReducer),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+  // Conditionally enable Redux DevTools only in development
+  devTools: process.env.NODE_ENV === "development",
+});
 
-  const persistor = persistStore(store);
+const persistor = persistStore(store);
 
-  const { dispatch } = store;
-  
-  const useSelector = useAppSelector;
-  
-  const useDispatch = () => useAppDispatch();
-  
-  export { store, persistor, dispatch, useSelector, useDispatch };
+const { dispatch } = store;
+
+const useSelector = useAppSelector;
+
+const useDispatch = () => useAppDispatch();
+
+export { store, persistor, dispatch, useSelector, useDispatch };
